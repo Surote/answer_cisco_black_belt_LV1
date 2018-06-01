30ec5a7f135502992934d97ead7aa9c31cb94680
Surote.Wongpaiboon <surote.z9@gmail.com>
2018-06-01 17:43:30 +0700

    edit: header

--- Camp1_Day2_assign1.py
+++ Camp1_Day2_assign1.py
@@ -1,2 +1,5 @@
+# Created: Surote Wongpaiboon
+# Email :  surote.wongpaiboon@dimensiondata.com
+# Tel :    +66-2625-0999
 Sessions_Attended = {'sessions' : '1011,2344,3222,44322,555,6332,721,8789,99,1011,1124,1245,137,1499'}
 print('I have attended '+str(len(Sessions_Attended['sessions'].split(',')))+' sessions!!')
--- Camp1_Day2_assign2.py
+++ Camp1_Day2_assign2.py
@@ -1,3 +1,6 @@
+# Created: Surote Wongpaiboon
+# Email :  surote.wongpaiboon@dimensiondata.com
+# Tel :    +66-2625-0999
 import random
 rand=random.randint(1,10)
 n=int(input('Guess the number: '))
